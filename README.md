a43a6115c4cd5b18e01dc2ca634cf7e45f5a8e50
Leon Noel <leon.a.noel@gmail.com>
2017-11-29 10:52:13 -0500

    first commit - added all

---
+++ README.md
@@ -0,0 +1,15 @@
+# Repo for Zell's CRUD, Express and MongoDB tutorial
+
+- [Demo](https://crud-express-mongo.herokuapp.com)
+- [Tutorial](http://zell-weekeat.com/crud-express-mongodb)
+
+## Installation
+
+1. Clone repo
+2. run `npm install` 
+
+## Usage 
+
+1. run `npm run dev`
+2. Navigate to `localhost:3000`
+3. Have fun ;)
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+MongoClient.connect('mongodb://demo:demo@ds125146.mlab.com:25146/savage', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('messages').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {messages: result})
+  })
+})
+
+app.post('/messages', (req, res) => {
+  db.collection('messages').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
