52b9e5c39c51dca8c09b0a80cd2cb2f1f51cd2da
Jasmin-L-Alvarez <ajasmin01@gmail.com>
2021-03-21 03:27:41 -0400

    update

--- public/main.js
+++ public/main.js
@@ -1,6 +1,11 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
 var trash = document.getElementsByClassName("fa-trash");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 
+// any thumb up will have a event listner 
+//will grab the name and the message 
+// client side which we will make a fetch request and a put method request
+// 
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
@@ -42,4 +47,33 @@ Array.from(trash).forEach(function(element) {
           window.location.reload()
         })
       });
+});
+
+// thumbdown 
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    // const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+
+
+    fetch('thumbDown', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        // 'thumbUp':thumbUp
+        'thumbDown':thumbDown
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
 });
