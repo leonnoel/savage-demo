1dc694bf9a853311af96dabdb612a07917e27327
Guthemberg Teixeira <guthembergteixeira@gmail.com>
2021-03-27 19:14:30 -0400

    Added thumbsDown functionality
    I completed the challenge: 5
    I feel good about my code: 4
    
    I want to fix the updating portion of it without it needing to refresh the page.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+var thumbDown = document.getElementsByClassName("fa-thumbs-down");
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
 var trash = document.getElementsByClassName("fa-trash");
 
@@ -6,7 +7,7 @@ Array.from(thumbUp).forEach(function(element) {
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
+        fetch('thumbUp', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -23,6 +24,30 @@ Array.from(thumbUp).forEach(function(element) {
           window.location.reload(true)
         })
       });
+});
+
+Array.from(thumbDown).forEach(function(element) {
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[7].innerText)
+    fetch('thumbDown', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbDown': thumbDown
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
 });
 
 Array.from(trash).forEach(function(element) {
