a44fa219f7634c824ff7579a1893c159375e756b
Shawna Mays <shawnadmays@gmail.com>
2022-11-02 20:50:24 -0400

    this is the start of my personal express

--- public/main.js
+++ public/main.js
@@ -1,45 +1,120 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+// var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+// var thumbDown = document.getElementsByClassName("fa-thumbs-down");
+var trash = document.getElementsByClassName("fa-trash-o");
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
+let birthChart = document.getElementsByClassName("sun","date")
+
+
+
+Array.from(birthChart).forEach(function (element) {
+
+  element.addEventListener('submit', function () {
+    const date = this.parentNode.parentNode.childNodes[1].innerText
+    const sun = this.parentNode.parentNode.childNodes[2].innerText
+    const moon = this.parentNode.parentNode.childNodes[3].innerText
+    const venus = this.parentNode.parentNode.childNodes[4].innerText
+
+    fetch('birthChart', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        'date': date,
+        'sun': sun,
+        'moon': moon,
+        'venus': venus
+
+      })
+    })
+      .then(response => {
+        if (response.ok) return response.json()
+      })
+      .then(data => {
+        console.log(data)
+        window.location.reload(true)
+      })
+  });
 });
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
+
+Array.from(trash).forEach(function (element) {
+  element.addEventListener('click', function () {
+    const date = this.parentNode.parentNode.childNodes[1].innerText
+    const sun = this.parentNode.parentNode.childNodes[2].innerText
+    const moon = this.parentNode.parentNode.childNodes[3].innerText
+    const venus = this.parentNode.parentNode.childNodes[4].innerText
+    fetch('birthChart', {
+      method: 'delete',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'date': date,
+        'sun': sun,
+        'moon': moon,
+        'venus': venus
+      })
+    }).then(function (response) {
+      window.location.reload()
+    })
+  });
 });
+
+
+
+//I'm going to get the sun sign by checking the value
+// of the date entered by the user
+
+
+
+// Array.from(thumbUp).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbUp
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+// // FETCH FOR THUMBS DOWN GOES HERE TO LOCATE IT----------------------------------------
+// Array.from(thumbDown).forEach(function(element) {
+//       element.addEventListener('click', function(){
+//         const name = this.parentNode.parentNode.childNodes[1].innerText
+//         const msg = this.parentNode.parentNode.childNodes[3].innerText
+//         // check the location that this directs to-------------------
+//         const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+//         fetch('messages', {
+//           method: 'put',
+//           headers: {'Content-Type': 'application/json'},
+//           body: JSON.stringify({
+//             'name': name,
+//             'msg': msg,
+//             'thumbUp':thumbDown - 2
+//           })
+//         })
+//         .then(response => {
+//           if (response.ok) return response.json()
+//         })
+//         .then(data => {
+//           console.log(data)
+//           window.location.reload(true)
+//         })
+//       });
+// });
+
+
+
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+
+// my goal is for user to input their date and they are given their signs based on the data from
+//the API.  they can then input it and ????
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
-
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
-const dbName = "demo";
+//URL from mongodb website cluster
+//MAKE SURE TO CHANGE THIS TO MISTYFOXSONG AND THE HOROSCOPE PROJECT
+const url = "mongodb+srv://mistyfoxsong:1234@cluster0.ndqi8wd.mongodb.net/Horoscope?retryWrites=true&w=majority";
+const dbName = "Horoscope";
 
 app.listen(3000, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
@@ -23,38 +27,122 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
+//this request is making a READ/GET request from the root and asking it to retrieve
+//everything it knows about the request and response.
+// it go into the database, into the collection called "messages" and find the data
+//turn the data into an Array. If there's an error, let us know, if not, render the index.ejs
+//page and display the object (document) from our database from messages collection and the result data
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+  db.collection('birthChart').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', {birthChart: result})
   })
 })
+//THIS WAS CHANGED TO birthChart
+app.post('/birthChart', (req, res) => {
+  function check() {
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+    let month = new Date(document.querySelector("#birthday").value).getMonth() + 1
+    let day = new Date(document.querySelector("#birthday").value).getDate() + 1
+  
+  
+  
+  
+    if (month == 1 && day >= 21 || month == 2 && day <= 22) {
+      sun = Aquarius
+    }
+  
+    else if (month == 2 && day >= 21 || month == 3 && day <= 22) {
+  
+      sun = Pisces
+    }
+  
+    else if (month == 3 && day >= 21 || month == 4 && day <= 22) {
+  
+      sun = Aries
+    }
+  
+    else if (month == 4 && day >= 21 || month == 5 && day <= 22) {
+  
+      sun = Taurus
+    }
+  
+    else if (month == 5 && day >= 21 || month == 6 && day <= 22) {
+      sun = Gemini
+  
+    }
+  
+  
+    else if (month == 6 && day >= 21 || month == 7 && day <= 22) {
+      sun = Cancer
+    }
+  
+    else if (month == 7 && day >= 21 || month == 8 && day <= 22) {
+      sun = Leo
+    }
+  
+    else if (month == 8 && day >= 21 || month == 9 && day <= 22) {
+      sun = Virgo
+    }
+  
+    else if (month == 9 && day >= 21 || month == 10 && day <= 22) {
+      sun = Libra
+    }
+  
+    else if (month == 10 && day >= 21 || month == 11 && day <= 22) {
+      sun = Scorpio
+    }
+  
+    else if (month == 11 && day >= 21 || month == 12 && day <= 22) {
+      sun = Sagittarius
+    }
+  
+    else if (month == 12 && day <= 22 || month == 1 && day <= 21) {
+      sun = Capricorn
+    }
+  
+  }
+  db.collection('birthChart').insertOne({date: req.body.date, sun: req.body.sun, moon: req.body.moon, venus: req.body.venus}, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
-app.put('/messages', (req, res) => {
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-})
+// app.put('/messages', (req, res) => {
+//   db.collection('messages')
+//   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+//     $set: {
+//       thumbUp:req.body.thumbUp + 1
+//     }
+//   }, {
+//     sort: {_id: -1},
+//     upsert: true
+//   }, (err, result) => {
+//     if (err) return res.send(err)
+//     res.send(result)
+//   })
+// })
 
-app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+//logic for thumbs down goes here =================
+// app.put('/messages', (req, res) => {
+//   db.collection('messages')
+//   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+//     $set: {
+      
+//       thumbDown:req.body.thumbDown - 1
+//     }
+//   }, {
+//     sort: {_id: -1},
+//     upsert: true
+//   }, (err, result) => {
+//     if (err) return res.send(err)
+//     res.send(result)
+//   })
+// })
+
+app.delete('/birthChart', (req, res) => {
+  db.collection('birthChart').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
     if (err) return res.send(500, err)
     res.send('Message deleted!')
   })
