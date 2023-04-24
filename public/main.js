6cedf65bef318123f735754c49e9ccb659f233f7
Julia Cunha <cunha.t.julia@gmail.com>
2023-04-24 11:43:46 -0400

    answer

--- public/main.js
+++ public/main.js
@@ -1,12 +1,13 @@
 var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var thumbDown = document.getElementsByClassName("fa-thumbs-down"); //added a new variable for thumbs down to start
 var trash = document.getElementsByClassName("fa-trash");
 
 Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const name = this.parentNode.parentNode.childNodes[1].innerText //'this' is used to access properties and methods of the object or html that 'this' refers to- in this case, we are noting parent(s)/child node relationships in the index.ejs file. In both thumbUp and thumbDown functions, we want to highlight the specific lines of code referring to where the message is displayed, and where the thumbs up and thumbs down icons are, so that we can trigger their functionality based upon user input (i.e. whether they thumb messages up/down)
         const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText) //parseFloat turns string into number(otherwise this would output NaN = Not a Number)
+        fetch('messages/thumbUp', { //important: we needed to indicate file path here specifically! This code then makes a PUT request to the URL 'messages/thumbUp', sending JSON data in the request body. The request includes the name, message, and thumbUp data as a JSON object. The header 'Content-Type' is set to 'application/json' to indicate that the request body is in JSON format.
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -16,14 +17,43 @@ Array.from(thumbUp).forEach(function(element) {
           })
         })
         .then(response => {
-          if (response.ok) return response.json()
+          if (response.ok) return response.json() //response.ok is a property of the response object returned by a fetch() request. It is a boolean value that indicates whether the request was successful or not. If response.ok is true, it means that the status code of the response is in the range 200-299 = successful request. The then() method is used to handle the response, and the code inside the if statement is executed only if the request was successful (i.e., response.ok is true).
         })
         .then(data => {
           console.log(data)
-          window.location.reload(true)
+          window.location.reload(true) //this is making the page automatically reload each time a user thumbs any messages up or down.
         })
       });
 });
+
+
+Array.from(thumbDown).forEach(function(element) { //copied code block for thumbUp to apply to thumbDown, with appropriate modifications 
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText //important: refer to index.ejs to make note of parent(s)/child node relationship (also noted above). We know that index count begins at 0, and skipped lines are text nodes created by the whitespace or line breaks in the index code. 
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[9].innerText) 
+    const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+    fetch('messages/thumbDown', { //important: we needed to indicate file path here specifically as well! 
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        'msg': msg,
+        'thumbDown':thumbDown,
+        'thumbUp': thumbUp,
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+  });
+});
+
+
 
 Array.from(trash).forEach(function(element) {
       element.addEventListener('click', function(){
--- server.js
+++ server.js
@@ -1,52 +1,70 @@
+// notes: after cloning this repo from Github, we open the terminal to get the additional node packages/etc. needed to successfully run this code and complete this project. 1) so, in the terminal we start with: npm install, npm install express --save. 2) check for all packages on left sidebar. 3) dependencies? mongoDB as part of the nodules? etc. 
+
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
-const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser') //middleware for processing HTTP request body data in a Node.js application. It parses the request body data into a usable format, such as JSON or URL-encoded data, and attaches it to the req.body object, making it accessible to the application's routes and handlers.
+const MongoClient = require('mongodb').MongoClient //require: dependency is loaded and has its functionality running. note: mongoDB is a module
 
-var db, collection;
+var db, collection; //note: var is not used here because its old code, its used because of scoping
 
-const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true";
+const url = "mongodb+srv://demo:demo@cluster0-q2ojb.mongodb.net/test?retryWrites=true"; //this is the url for the mongoDB acct holding all this data. When we create our own mongoDB accounts, mongo will give us our own url to paste into this server portion of our code. 
 const dbName = "demo";
 
-app.listen(3000, () => {
+app.listen(3000, () => { //listen for the localhost port we are choosing (in this case, port 3000)
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
         if(error) {
             throw error;
         }
         db = client.db(dbName);
-        console.log("Connected to `" + dbName + "`!");
+        console.log("Connected to `" + dbName + "`!"); //i.e. this is what we see to know that when we run node server, this msg tells us its running successfully. you will want to include a console.log like this for your projects as a helpful visual
     });
 });
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') //used to set the default view engine for the application to EJS (Embedded JavaScript). This means that when a route handler renders a view, Express will look for an EJS template file with the same name as the view and use it to render the HTML output that will be sent back to the client.
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { //indicating file path we are seeking to get. now note CRUD here and in lines below
   db.collection('messages').find().toArray((err, result) => {
     if (err) return console.log(err)
     res.render('index.ejs', {messages: result})
   })
 })
 
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+app.post('/messages', (req, res) => { 
+  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0}, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
-app.put('/messages', (req, res) => {
+app.put('/messages/thumbUp', (req, res) => { //we added specific file paths to our arrays in main.js, necessary here and for thumbDown as well 
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
       thumbUp:req.body.thumbUp + 1
     }
   }, {
-    sort: {_id: -1},
-    upsert: true
+    sort: {_id: -1}, //sorting from bottom to top in terms of where to get things from 
+    upsert: true //in lieu of the above, if nothing, then it will create a new doc to serve this purpose 
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+
+app.put('/messages/thumbDown', (req, res) => { //note file path here (after messages/)
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp - 1
+    }
+  }, {
+    sort: {_id: -1}, //sorting from bottom to top in terms of where to get things from 
+    upsert: true //in lieu of the above, if nothing, then it will create a new doc to serve this purpose 
   }, (err, result) => {
     if (err) return res.send(err)
     res.send(result)
